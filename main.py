1f54a8fb13a8ca960900e12f843524cc398e1be3
Joshua Mutavi <alolagamerza@gmail.com>
2026-09-08 16:41:01 -0500

    Add color swatch display

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Color Palette Extractor
+
+Open any image and pull out its dominant colors as a palette of swatches
+with hex codes you can click to copy. Uses a small hand-rolled k-means so
+the only dependencies are Pillow and numpy -- no heavier vision libraries
+needed.
+"""
